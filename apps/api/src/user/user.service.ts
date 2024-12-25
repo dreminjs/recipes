818a23c5b8cb9bdec8da35f9f87774dc3c71b162
Andrey Dremin <sankoa230@gmail.com>
2024-12-26 00:58:35 +0300

    init

---
+++ apps/api/src/user/user.service.ts
@@ -0,0 +1,21 @@
+import { Body, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma';
+import { Prisma, User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async createOne(@Body() body: CreateUserDto): Promise<User> {
+    return await this.prisma.user.create({
+      data: { ...body },
+    });
+  }
+
+  public async findOne(where: Prisma.UserWhereUniqueInput ): Promise<User> {
+    return await this.prisma.user.findUnique({
+      where,
+    });
+  }
+}
