a69aca4fc6bc123d2e7f219ac10cb168f765db82
Andrey Dremin <sankoa230@gmail.com>
2024-12-26 20:35:30 +0300

    feat: post recipe

---
+++ apps/api/src/recipe/recipe-photo.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+
+
+
+
+export const RecipePhoto = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+    return ctx.switchToHttp().getRequest().file
+})
--- apps/api/src/recipe/recipe.controller.ts
+++ apps/api/src/recipe/recipe.controller.ts
@@ -1,23 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { Recipe, User } from '@prisma/client';
 import { RecipeService } from './recipe.service';
 import { CurrentUser } from '../user';
+import { RecipePhoto } from './recipe-photo.decorator';
+import { AccessTokenGuard } from '../token';
+
 
 @Controller('recipe')
 export class RecipeController {
   constructor(private readonly recipeService: RecipeService) {}
 
+  @UseGuards(AccessTokenGuard)
   @Post()
   public async createOne(
     @Body() body: CreateRecipeDto,
-    @CurrentUser() user: User
+    @CurrentUser() user: User,
+    @RecipePhoto() photo: string
   ): Promise<Recipe> {
     
 
     const recipe = await this.recipeService.createOne({
       title: body.title,
       description: body.description,
+      photo,
       ingredients: {
         createMany: {
           data: body.ingredients,
