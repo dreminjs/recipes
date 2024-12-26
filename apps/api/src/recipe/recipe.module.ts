938f4f28dd02238a8bd01fed4d8226deea3446b2
Andrey Dremin <sankoa230@gmail.com>
2024-12-26 20:32:21 +0300

    feat: file upload middleware

--- apps/api/src/recipe/recipe.module.ts
+++ apps/api/src/recipe/recipe.module.ts
@@ -1,13 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { RecipeController } from './recipe.controller';
 import { RecipeService } from './recipe.service';
 
 import { PrismaModule } from '../prisma';
 import { MulterModule } from '@nestjs/platform-express';
+import { UploadPhotoMiddleware } from './middlewares/recipe-photo-upload.middleware';
 
 @Module({
   imports: [PrismaModule, MulterModule.register({ dest: './assets' })],
   controllers: [RecipeController],
   providers: [RecipeService],
 })
-export class RecipeModule {}
+export class RecipeModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UploadPhotoMiddleware)
+      .forRoutes({ path: 'images', method: RequestMethod.POST })
+      .apply(UploadPhotoMiddleware).forRoutes({ path: 'images', method: RequestMethod.PUT })
+      .apply(UploadPhotoMiddleware).forRoutes({ path: 'images', method: RequestMethod.PATCH });
+  }
+}
