cfa5daeb549c7a7e7145df34d4efed4d66b033d9
andrey_dremin <sankoa230@gmail.com>
2025-01-29 19:29:43 +0300

    chore: move to other folder

--- apps/client/src/widgets/admin/index.ts
+++ apps/client/src/widgets/admin/index.ts
@@ -2,4 +2,4 @@ export { AdminPostIngredient } from './ui/ingredient/admin-post-ingredient';
 
 export { AdminIngredientsTable } from './ui/ingredient/admin-ingredients-table';
 
-export { AdminCharacteristicsTable } from "./ui/ingredient/admin-characteristics-table"
+export { AdminCharacteristicsTable } from "./ui/characteristic/admin-characteristics-table"
