7e912d5b8ffc95999330453fb05d8de196b700df
andrey_dremin <sankoa230@gmail.com>
2025-07-05 20:12:46 +0300

    feat: confirm email label

--- apps/client/src/pages-components/profile/ui/current-user-page.tsx
+++ apps/client/src/pages-components/profile/ui/current-user-page.tsx
@@ -2,7 +2,6 @@ import { ProfileInfoItem } from '@/features/user/ui/profile-info-item';
 import { ProfileCard } from '@/features/user';
 import { useGetMyProfile } from '../model/api/queries';
 import { AvatarInitial } from '../model/ui/icons/avatar-initial';
-import { EmailIcon } from '../model/ui/icons/email-icon';
 import { ToggleTwoFaStatus } from '@/featuresuser/ui/toggle-2fa-status';
 
 export const CurrentUserPage = () => {
@@ -15,11 +14,17 @@ export const CurrentUserPage = () => {
         label="Имя пользователя"
         value={userInfo?.nickname}
       />
-      
+
       <ProfileInfoItem
-        icon={<EmailIcon />}
+        icon={<img src='/email-icon.svg' className='w-[25px] h-[25px]' alt='email' />}
         label="Email"
         value={userInfo?.email}
+        fallback="Не указан"
+      />
+      <ProfileInfoItem
+        icon={<img src='/confirm-email-icon.svg' className='w-[25px] h-[25px]' alt='email' />}
+        label="Подверждение почты"
+        value={userInfo?.isActived ? "Подтвержден" : "Подтвердите"}
         fallback="Не указан"
       />
       <ToggleTwoFaStatus isEnabled={userInfo?.isTwoFactorEnabled} />
